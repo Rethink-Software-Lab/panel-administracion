6834520c6cd4ba6c29f44be4575aa351b6814e2f
Teby00 <tokkenpage@gmail.com>
2024-09-13 16:56:02 -0400

    corrijo error de hidratacion

--- app/(with-layout)/entradas/columns.jsx
+++ app/(with-layout)/entradas/columns.jsx
@@ -8,13 +8,26 @@ export const columns = [
   {
     accessorKey: 'created_at',
     header: 'Fecha',
-    cell: ({ row }) => (
-      <span>
-        {DateTime.fromISO(row.getValue('created_at')).toRelative({
-          locale: 'es',
-        })}
-      </span>
-    ),
+    cell: ({ row }) => {
+      const date = DateTime.fromISO(row.getValue('created_at'));
+      const now = DateTime.now();
+      const diff = now.diff(date, 'days').days;
+
+      if (diff < 1) {
+        return <span>hoy</span>;
+      } else if (diff < 2) {
+        return <span>ayer</span>;
+      } else {
+        return (
+          <span>
+            {date.toRelative({
+              unit: 'day',
+              locale: 'es',
+            })}
+          </span>
+        );
+      }
+    },
   },
   {
     accessorKey: 'producto__info__descripcion',
