6b317ddb8e27a8e4f7ed277ed03e87d1f29fcac1
Teby00 <tokkenpage@gmail.com>
2024-10-10 05:59:06 -0400

    🧹 Limpiando y optimizando llamadas

--- app/(with-layout)/page.tsx
+++ app/(with-layout)/page.tsx
@@ -7,38 +7,22 @@ import CardMasVendidos from '@/components/functionals/CardMasVendidos';
 import ChartVentaPorArea from '@/components/functionals/ChartVentasPorArea';
 import ChartVentasAnuales from '@/components/functionals/ChartVentasAnuales';
 
-import { Skeleton } from '@/components/ui/skeleton';
-
-import {
-  ventasHoy,
-  ventasSemana,
-  ventasMes,
-  graficoVentasAnuales,
-  ventasPorArea,
-} from '@/lib/services';
-import { Suspense } from 'react';
+import { graficas } from '@/lib/services';
 import { getSession } from '@/lib/getSession';
 import { redirect } from 'next/navigation';
 
 export default async function Initial() {
   const { isVendedor, punto } = getSession();
-  const { data: ventas } = await ventasPorArea();
+  const { data } = await graficas();
   if (isVendedor) return redirect(`areas-de-venta/${punto}`);
-  const { data, error } = await graficoVentasAnuales();
 
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
       <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
-        <Suspense fallback={<Skeleton className="rounded-lg" />}>
-          <CardsInicio description="Hoy" getData={ventasHoy} />
-        </Suspense>
-        <Suspense fallback={<Skeleton className="rounded-lg" />}>
-          <CardsInicio description="Esta semana" getData={ventasSemana} />
-        </Suspense>
-        <Suspense fallback={<Skeleton className="rounded-lg" />}>
-          <CardsInicio description="Este mes" getData={ventasMes} />
-        </Suspense>
-        <Card className=" opacity-50 ">
+        <CardsInicio description="Hoy" data={data?.ventasHoy} />
+        <CardsInicio description="Esta semana" data={data?.ventasSemana} />
+        <CardsInicio description="Este mes" data={data?.ventasMes} />
+        <Card className="opacity-50 ">
           <CardTitle className="flex text-lg gap-2 items-center p-6 opacity-50">
             <Hourglass className="" size={20} />
             Próximamente
@@ -46,11 +30,11 @@ export default async function Initial() {
         </Card>
       </div>
       <div className="grid grid-cols-3 gap-6">
-        <ChartVentaPorArea data={ventas} />
+        <ChartVentaPorArea data={data?.ventasPorArea} />
 
-        <CardMasVendidos />
+        <CardMasVendidos data={data?.masVendidos} />
       </div>
-      <ChartVentasAnuales data={data} />
+      <ChartVentasAnuales data={data?.ventasAnuales} />
     </main>
   );
 }
---
+++ components/functionals/CardMasVendidos.tsx
@@ -0,0 +1,84 @@
+import { FileX, FolderXIcon } from 'lucide-react';
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+import Image from 'next/image';
+import { Categoria } from '@/app/(with-layout)/categorias/types';
+
+interface Imagen {
+  public_id: string;
+  url: string;
+}
+
+interface Producto {
+  imagen: Imagen | null;
+  categoria: Categoria;
+  id: number;
+  codigo: string;
+  descripcion: string;
+  pago_trabajador: number;
+  precio_costo: number;
+  precio_venta: number;
+}
+
+interface Props {
+  producto: Producto;
+  cantidad: number;
+}
+
+export default async function CardMasVendidos({ data }: { data: Props[] }) {
+  return (
+    <>
+      <Card className="col-span-3 md:col-span-1">
+        <CardHeader>
+          <CardTitle>Productos más vendidos</CardTitle>
+        </CardHeader>
+        <CardContent className="h-full">
+          {data.length < 1 ? (
+            <div className="flex justify-center items-center h-3/4 py-4">
+              <div className="flex flex-col items-center text-muted-foreground">
+                <FolderXIcon size={50} />
+                <h3 className="font-medium">
+                  Cuando existan datos aparecerán aquí.
+                </h3>
+              </div>
+            </div>
+          ) : (
+            <ul className="flex flex-col gap-2">
+              {data?.map((prod: Props) => (
+                <li
+                  key={prod.producto?.codigo}
+                  className="flex items-center justify-between hover:bg-muted transition-all px-3 py-2  rounded-lg "
+                >
+                  <div className="flex items-center gap-2">
+                    {prod.producto?.imagen?.url ? (
+                      <Image
+                        className="rounded-lg object-cover aspect-square"
+                        src={prod.producto.imagen.url}
+                        width={40}
+                        height={40}
+                        alt="venta"
+                      />
+                    ) : (
+                      <div className="rounded-lg object-cover aspect-square w-10 h-10 border text-muted-foreground bg-muted flex justify-center items-center">
+                        <FileX size={20} />
+                      </div>
+                    )}
+                    <div>
+                      <p className="font-medium text-sm">
+                        {prod.producto?.codigo}
+                      </p>
+                      <p className="text-sm">{prod.producto?.descripcion}</p>
+                    </div>
+                  </div>
+                  <div>
+                    <p className="font-medium text-right">{prod.cantidad}</p>
+                    <p className="text-[.8rem] font-light">unidades</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </CardContent>
+      </Card>
+    </>
+  );
+}
