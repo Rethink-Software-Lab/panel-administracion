897c99b7b4e51e26acb293fd2bad1f114b50c3b6
Teby00 <tokkenpage@gmail.com>
2024-10-24 17:35:56 -0400

    🪲 arreglando bug de reportes

--- app/(with-layout)/reportes/page.tsx
+++ app/(with-layout)/reportes/page.tsx
@@ -25,13 +25,11 @@ export default async function Reportes({
   const { data: areas } = await getAreasVentas();
   const { data: reportes, error } = await getReporte(searchParams);
 
-  console.log(reportes);
-
   return (
     <main className="flex flex-1 flex-col pt-4 lg:pt-6">
       <div className="flex flex-col gap-4 border-b border-b-gray-200 pb-4 px-4 lg:px-6">
         <h1 className="text-lg font-semibold md:text-2xl sm:pb-2 lg:pb-4">
-          Reportes de ventas
+          Reportes
         </h1>
         <div className="flex items-center justify-between max-sm:block max-sm:space-y-2">
           <FormReportes areas={areas} />
