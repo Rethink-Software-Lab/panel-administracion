e8cf599690017382f9d741d3e0795ac12bf38ee2
Esteban <estebarmartin@gmail.com>
2025-08-07 02:27:24 -0400

    migrando get ajuste inventario

--- app/(with-layout)/ajuste-inventario/columns.tsx
+++ app/(with-layout)/ajuste-inventario/columns.tsx
@@ -1,31 +1,32 @@
-'use client';
-import TableDeleteV2 from '@/components/functionals/TableDeleteV2';
-import { deleteAjuste } from '@/app/(with-layout)/ajuste-inventario/actions';
-import { DateTime } from 'luxon';
-import { ColumnDef } from '@tanstack/react-table';
-import SheetInfoAjusteInventario from '@/components/functionals/SheetInfoAjusteInventario';
-import { AjusteInventario } from '@/app/(with-layout)/ajuste-inventario/types';
-import { Badge } from '@/components/ui/badge';
+"use client";
+import TableDeleteV2 from "@/components/functionals/TableDeleteV2";
+import { deleteAjuste } from "@/app/(with-layout)/ajuste-inventario/actions";
+import { DateTime } from "luxon";
+import { ColumnDef } from "@tanstack/react-table";
+import SheetInfoAjusteInventario from "@/components/functionals/SheetInfoAjusteInventario";
+import { AjusteInventario } from "@/app/(with-layout)/ajuste-inventario/types";
+import { Badge } from "@/components/ui/badge";
 
 export const columns: ColumnDef<AjusteInventario>[] = [
   {
-    accessorKey: 'created_at',
-    header: 'Fecha',
+    accessorKey: "created_at",
+    header: "Fecha",
     cell: ({ row }) =>
-      DateTime.fromISO(row.getValue('created_at')).toLocaleString(
+      DateTime.fromSQL(row.getValue("created_at")).toLocaleString(
         DateTime.DATETIME_MED,
-        { locale: 'es' }
+        { locale: "es" }
       ),
   },
   {
-    accessorKey: 'motivo',
-    header: 'Motivo',
+    accessorKey: "motivo",
+    header: "Motivo",
+    size: 800,
   },
   {
-    accessorKey: 'usuario.username',
-    header: 'Usuario',
+    accessorKey: "usuario.username",
+    header: "Usuario",
     cell: ({ row }) => {
-      const username = row.original.usuario?.username;
+      const username = row.original.usuario;
       if (username) {
         return username;
       } else {
@@ -35,7 +36,7 @@ export const columns: ColumnDef<AjusteInventario>[] = [
   },
 
   {
-    header: ' ',
+    header: " ",
     cell: ({ row }) => (
       <span className="flex gap-2 justify-center">
         <SheetInfoAjusteInventario data={row.original} />
